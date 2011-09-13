1484d6d5f9a8b42818403ac210aff751924dc1b2
Forrest L Norvell <ogd@aoaioxxysz.net>
2011-09-12 23:48:33 -0700

    Many stylistic tweaks to match LockerProject script style.

--- lockerbox.sh
+++ lockerbox.sh
@@ -49,12 +49,14 @@ check_for() {
     fi
 }
 
-function download {
-    base=`basename $1`
-    if [ -f $base ]; then
+download () {
+    base="$(basename $1)"
+    if [ -f ${base} ]
+    then
         echo "$1 already downloaded." >&2
     else
-        if wget "$1" 2>/dev/null || curl -L -o $base "$1"; then
+        if wget "$1" 2>/dev/null || curl -L -o ${base} "$1"
+        then
             echo "Downloaded $1." >&2
         else
             echo "Download of $1 failed!" >&2
@@ -89,56 +91,64 @@ cd local/build
 
 check_for Node.js node 'node -v' 0.4.10 optional
 
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]
+then
     echo "" >&2
     echo "About to download, build, and install locally node.js." >&2
     echo -n "This could take a while." >&2
-    sleep 1; echo -n .; sleep 1; echo -n .; sleep 1; echo -n .; sleep 1
-    download "$NODE_DOWNLOAD"
-    if tar zxf "`basename \"$NODE_DOWNLOAD\"`" &&
-        cd `basename "$NODE_DOWNLOAD" .tar.gz` &&
-        ./configure --prefix="$BASEDIR/local" &&
+    sleep 1 ; printf "." ; sleep 1 ; printf "." ; sleep 1 ; printf "." ; sleep 1
+    download "${NODE_DOWNLOAD}"
+    if tar zxf "$(basename \"${NODE_DOWNLOAD}\")" &&
+        cd $(basename "${NODE_DOWNLOAD}" .tar.gz) &&
+        ./configure --prefix="${BASEDIR}/local" &&
         make &&
         make install
     then
-        echo "Installed node.js into $BASEDIR" >&2
+        echo "Installed node.js into ${BASEDIR}" >&2
     else
-        echo "Failed to install node.js into $BASEDIR" >&2
+        echo "Failed to install node.js into ${BASEDIR}" >&2
         exit 1
     fi
 fi
 
-cd "$BASEDIR/local/build"
+cd "${BASEDIR}/local/build"
 check_for npm npm "npm -v" 1 optional
 
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]
+then
     echo "" >&2
     echo "About to download and install locally npm." >&2
-    download "$NPM_DOWNLOAD" 
-    if cat `basename $NPM_DOWNLOAD` | clean=no sh; then
-        echo "Installed npm into $BASEDIR" >&2
+    download "${NPM_DOWNLOAD}"
+    if cat "$(basename ${NPM_DOWNLOAD})" | clean=no sh
+    then
+        echo "Installed npm into ${BASEDIR}" >&2
     else
-        echo "Failed to install npm into $BASEDIR" >&2
+        echo "Failed to install npm into ${BASEDIR}" >&2
         exit 1
     fi
 fi
 
-if ! find "$BASEDIR/local/bin/activate" >/dev/null 2>&1; then
+if ! find "${BASEDIR}/local/bin/activate" >/dev/null 2>&1
+then
     check_for virtualenv virtualenv "virtualenv --version" 1.4 optional
 
-    if [ $? -ne 0 ]; then
+    if [ $? -ne 0 ]
+    then
         echo "" >&2
         echo "About to download virtualenv.py." >&2
-        download "$VIRTUALENV_DOWNLOAD"
+        download "${VIRTUALENV_DOWNLOAD}"
     fi
-    if $PYEXE -m virtualenv --no-site-packages "$BASEDIR/local"; then
+
+    if ${PYEXE} -m virtualenv --no-site-packages "${BASEDIR}/local"
+    then
         echo "Set up virtual Python environment." >&2
     else
         echo "Failed to set up virtual Python environment." >&2
     fi
 fi
 
-if source "$BASEDIR/local/bin/activate"; then
+if . "${BASEDIR}/local/bin/activate"
+then
     echo "Activated virtual Python environment." >&2
 else
     echo "Failed to activate virtual Python environment." >&2
@@ -146,9 +156,10 @@ fi
 
 check_for mongoDB mongod "mongod --version" 1.4.0 optional
 
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]
+then
     OS=`uname -s`
-    case $OS in
+    case "${OS}" in
         Linux)
             OS=linux
             ;;
@@ -156,23 +167,25 @@ if [ $? -ne 0 ]; then
             OS=osx
             ;;
         *)
-            echo "Don't recognize OS $OS" >&2
+            echo "Don't recognize OS ${OS}" >&2
             exit 1
     esac
     BITS=`getconf LONG_BIT`
     ARCH='x86_64'
-    if [ $BITS -ne 64 ]; then
+    if [ "${BITS}" -ne 64 ]
+    then
         ARCH="i386"
-        if [ "$OS" != "osx" ]; then
+        if [ "${OS}" != "osx" ]
+        then
             ARCH="i686"
         fi
     fi
     echo "" >&2
     echo "Downloading and installing locally mongoDB" >&2
-    MONGODB_DOWNLOAD=`echo $MONGODB_DOWNLOAD | sed -e "s/OS/$OS/g" -e "s/ARCH/$ARCH/g"`
-    download $MONGODB_DOWNLOAD
-    if tar zxf `basename "$MONGODB_DOWNLOAD"` &&
-        cp `basename "$MONGODB_DOWNLOAD" .tgz`/bin/* "$BASEDIR/local/bin"
+    MONGODB_DOWNLOAD=$(echo ${MONGODB_DOWNLOAD} | sed -e "s/OS/${OS}/g" -e "s/ARCH/${ARCH}/g")
+    download "${MONGODB_DOWNLOAD}"
+    if tar zxf $(basename "${MONGODB_DOWNLOAD}") &&
+        cp $(basename "${MONGODB_DOWNLOAD}" .tgz) /bin/* "${BASEDIR}/local/bin"
     then
         echo "Installed local mongoDB." >&2
     else
@@ -181,17 +194,20 @@ if [ $? -ne 0 ]; then
     fi
 fi
 
-cd "$BASEDIR"
+cd "${BASEDIR}"
 
-if [ ! -d Locker/.git ]; then
+if [ ! -d Locker/.git ]
+then
     echo "Checking out Locker repo." >&2
-    if git clone "$LOCKER_REPO" -b "$LOCKER_BRANCH"; then
+    if git clone "${LOCKER_REPO}" -b "${LOCKER_BRANCH}"
+    then
         echo "Checked out Locker repo." >&2
     else
         echo "Failed to check out Locker repo." >&2
         exit 1
     fi
 fi
+
 cd Locker
 npm install
 python setupEnv.py
